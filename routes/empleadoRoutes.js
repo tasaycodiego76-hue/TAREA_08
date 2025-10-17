cca63eec10bd78ee28e7c87bfab34c3bacf76d88
Diego Tasayco Huaccamayta <tasaycodiego76@gmail.com>
2025-10-17 15:57:07 -0500

    Actualizado

--- routes/empleadoRoutes.js
+++ routes/empleadoRoutes.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()
 const empleadoController = require('../controllers/empleadoController')
 
-// CRUD básico
-router.get('/', empleadoController.obtenerTodos)   // GET - listar todos
-router.post('/', empleadoController.crear)        // POST - crear
-router.put('/:id', empleadoController.actualizar) // PUT - actualizar
+// CRUD 
+router.get('/', empleadoController.obtenerTodos)   // GET - listar 
+router.post('/', empleadoController.crear)         // POST - crear
+router.put('/:id', empleadoController.actualizar)  // PUT - actualizar
 router.delete('/:id', empleadoController.eliminar) // DELETE - eliminar
 
 module.exports = router
